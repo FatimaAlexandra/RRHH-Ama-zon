0c1a43b0c1f98482e90022c8c57adeea46945def
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2023-05-29 02:37:23 -0600

    feat:  platilla inicial CRUD sede

---
+++ Controllers/SedeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using amazon.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using amazon.Models.Inputs;
+using amazon.Models.Outputs;
+
+namespace amazon.Controllers
+{
+    [Route("[controller]")]
+    public class SedeController : Controller
+    {
+
+        DbamazonContext context = new DbamazonContext();
+        public IActionResult Index()
+        {
+            List<Sede> sedes = context.Sedes.Include(s => s.Pais).ToList();
+            List<Paise> paises = context.Paises.ToList();
+            ViewBag.Sedes = sedes;
+            ViewBag.Paises = paises;
+            return View();
+        }
+    }
+}
